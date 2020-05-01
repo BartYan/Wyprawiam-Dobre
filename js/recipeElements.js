cff9209f14ca11c64e97632990216f0950f89f7f
Bartosz Janusz <bartosz.yan@gmail.com>
2020-05-01 19:22:41 +0200

    mobile swipe for recipe class - update

--- js/recipeElements.js
+++ js/recipeElements.js
@@ -1,21 +1,114 @@
-const recipeElements = document.getElementsByClassName('recipe');
-    // const contentElements = document.getElementsByClassName('recipe_content');
+var currentItem = 0;
+var items;
+var container = document.querySelector(".recipe");
 
-    // convert HTML collection to array
-    const recipeElementsArray = [].slice.call(recipeElements);
-    //const contentElementsArray = [].slice.call(contentElements);
+function setup() {
+  items = document.querySelectorAll(".recipe_overlay");
 
-    recipeElementsArray.forEach(function (element) {
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i]; //do usunuiecia
+    item.style.zIndex = 1000 - i; //do usuniecia
+  }
+}
+setup();
 
-      // add hover style
-      element.addEventListener('touchmove', function (e) {
-        e.target.classList.add('recipe_overlay-mobile'); // or whichever class name you'd like 
-        e.target.classList.add('recipe_title-mobile');
-      });
+container.addEventListener("keydown", navigateCarousel, false);
+container.addEventListener("touchstart", startTouch, false);
+container.addEventListener("touchmove", moveTouch, false);
 
-      // remove hover style on end
-      element.addEventListener('touchend', function (e) {
-        e.target.classList.remove('recipe_overlay-mobile'); // or whichever class name you'd like
-        e.target.classList.remove('recipe_title-mobile');
-      });
-    });
+// Keyboard
+function navigateCarousel(e) {
+  var key = e.keyCode;
+  if (key === 37) {
+    previousItem();
+  } else if (key === 39) {
+    nextItem();
+  }
+}
+
+// Swipe Left / Right
+var initialX = null;
+
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+}
+
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+
+  var currentX = e.touches[0].clientX;
+  var diffX = initialX - currentX;
+
+  if (diffX > 0) {
+    nextItem();
+  } else {
+    previousItem();
+  }
+
+  initialX = null;
+
+  e.preventDefault();
+}
+
+function previousItem() {
+  if (currentItem > 0) {
+    currentItem--;
+
+    var item = items[currentItem];
+
+    item.classList.remove('recipe_overlay-mobile');
+
+  } else {
+    currentItem = 0;
+  }
+}
+
+function nextItem() {
+  if (currentItem < items.length - 1) {
+    var item = items[currentItem];
+
+    item.classList.add('recipe_overlay-mobile');
+
+
+    currentItem++;
+  } else {
+    currentItem = items.length - 1;
+  }
+}
+
+//do usunięcia
+function getRotation() {
+  return Math.round(-3 + Math.random() * 7);
+}
+
+
+
+
+
+
+//CODE DOWN BELOW
+//RECIPE OVERLAY SWIPE LEFT AND GO BACK TO RIGHT WHEN FINGER OFF
+
+//const recipeElements = document.getElementsByClassName('recipe');
+// const contentElements = document.getElementsByClassName('recipe_content');
+
+// convert HTML collection to array
+// const recipeElementsArray = [].slice.call(recipeElements);
+//const contentElementsArray = [].slice.call(contentElements);
+
+// recipeElementsArray.forEach(function (element) {
+
+// add hover style
+//  element.addEventListener('touchmove', function (e) {
+//    e.target.classList.add('recipe_overlay-mobile'); // or whichever class name you'd like 
+//    e.target.classList.add('recipe_title-mobile');
+//  });
+
+// remove hover style on end
+//   element.addEventListener('touchend', function (e) {
+//  e.target.classList.remove('recipe_overlay-mobile'); // or whichever class name you'd like
+//  e.target.classList.remove('recipe_title-mobile');
+// });
+//  });
