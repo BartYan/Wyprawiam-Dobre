65057bd1d509053756d6a73cc06d3c276855d114
Bartosz Janusz <bartosz.yan@gmail.com>
2020-11-08 21:37:13 +0100

    infoBox carousel prototype

---
+++ js/carousel.js
@@ -0,0 +1,43 @@
+const Slide = document.querySelector('.carousel-slide');
+const Images = document.querySelectorAll('.carousel-slide img');
+
+//buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//counter
+let counter = 1;
+const size = Images[0].clientWidth;
+
+Slide.style.transform = `translateX(${-size * counter}px)`;
+
+//button listeners
+
+nextBtn.addEventListener('click', () => {
+  if (counter >= Images.length - 1) return;
+  Slide.style.transition = "transform 0.6s ease-in-out";
+  counter++;
+  Slide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return;
+  Slide.style.transition = "transform 0.6s ease-in-out";
+  counter--;
+  Slide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+Slide.addEventListener('transitionend', () => {
+  const cloneOne = document.getElementById('lastCLone');
+  const cloneTwo = document.getElementById('firstCLone');
+
+  if (Images[counter] === cloneOne) {
+    Slide.style.transition = "none";
+    counter = Images.length - 2;
+    Slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  } else if (Images[counter] === cloneTwo) {
+    Slide.style.transition = "none";
+    counter = Images.length - counter;
+    Slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
